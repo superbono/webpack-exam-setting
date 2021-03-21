5a7b9d244e5b7ba7e4a998ea2481d67688cf98bd
superbono <hiperbono@naver.com>
2021-03-21 17:55:56 +0900

    html 경로변경 및 포트변경

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
   plugins : [
     new HtmlWebpackPlugin({
       // index.html 템플릿을 기반으로 빌드 결과물을 추가해줌
-      template: './src/index.html',
+      template: './public/index.html',
       filename: 'index.html' // output으로 출력할 파일은 index.html 이다.
       /*filename : test.html로 할 경우 => http://localhost:9000/test.html*/
     }),
@@ -28,6 +28,9 @@ module.exports = {
   output : {
     filename : "bundle.js",
     path : path.resolve (__dirname + "/build")
+  },
+  devServer : {
+    port: 9000
   },
   mode : "none"
 }
