de79b118b0e4d69f01ec706a7b9b7a0512f23c2e
superbono <hiperbono@naver.com>
2021-03-21 17:48:46 +0900

    second

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry : "./src/index.js",
+  module : {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      }
+    ]
+  },
+  plugins : [
+    new HtmlWebpackPlugin({
+      // index.html 템플릿을 기반으로 빌드 결과물을 추가해줌
+      template: './src/index.html',
+      filename: 'index.html' // output으로 출력할 파일은 index.html 이다.
+      /*filename : test.html로 할 경우 => http://localhost:9000/test.html*/
+    }),
+  ],
+  output : {
+    filename : "bundle.js",
+    path : path.resolve (__dirname + "/build")
+  },
+  mode : "none"
+}
