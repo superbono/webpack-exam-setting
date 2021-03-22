f0093580dad2a390567e430b6234a5b92e564449
superbono <choiar@pincar.co.kr>
2021-03-23 01:05:31 +0900

    Appstate 컨포넌트 정리

--- src/components/AppState.js
+++ src/components/AppState.js
@@ -9,7 +9,7 @@ export class AppState extends Component {
   render () {
     return (
       <div className="AppState">
-        <p>{`현재 리액트 ${this.props.text}를 공부하고 있습니다. `}</p>
+        <p>{`현재 리액트 ${this.props.text}를 공부하고 있습니다.`}</p>
       </div>
     )
   }
